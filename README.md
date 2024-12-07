612b1e0944522c9ee981cbc7b524f4c985b0f402
Muhammad Umar <umar43umar@gmail.com>
2024-12-07 16:45:30 +0500

    InitialCommit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ng-ToDo List App
+
+
+A mini project to create a simple todo List app using Angular, Typescript and Firebase!
+
+![](https://skillicons.dev/icons?i=angular)
+
+
+---------
+
+## [ Live Demo]()
+
+
+![Screen Shot 2023-02-27 at 01 10 58](https://user-images.githubusercontent.com/76174247/221446083-8973ce7f-2b91-4a3d-9058-1f6632f64265.png)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { TodoComponent } from './components/todo/todo.component';
+
+@NgModule({
+  declarations: [AppComponent, TodoComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
